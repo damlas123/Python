68cf6996b46b3b52d9b26595e4117dd8a1bf9f99
damlas123 <damlasoylemez321@gmail.com>
2024-11-08 02:09:58 +0300

    Update 1.py

--- DATA STRUCTURE/yapay.p/1.py
+++ DATA STRUCTURE/yapay.p/1.py
@@ -5,6 +5,6 @@ from transformers import pipeline
 pipe = pipeline("fill-mask", model="bert-base-uncased")
 
 # Farklı cümlelerde maske kelimeleri dene
-results = pipe("I didn't feel good . Am I [MASK] ?", top_k=10)  # En iyi 5 sonucu al
+results = pipe("I am feel good . Am I [MASK] ?", top_k=20)  # En iyi 5 sonucu al
 for result in results:
     print(f"Word: {result['token_str']}, Score: {result['score']}")
